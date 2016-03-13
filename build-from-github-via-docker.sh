28f11e5ec5423b6bb4531dc2bf5924e2ef292e9b
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-13 17:23:55 +1100

    Added script

---
+++ build-from-github-via-docker.sh
@@ -0,0 +1,7 @@
+BRANCH=$1
+
+if [ -z "$BRANCH" ]; then
+    BRANCH="master"
+fi
+
+docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/datamanager/$BRANCH/build-from-github.sh | sh -s $BRANCH"
