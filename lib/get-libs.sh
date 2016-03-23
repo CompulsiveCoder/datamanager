06cdc05b77dfe266567690022be9d3ddbdd1bc45
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-23 14:30:00 +1100

    Fixed json reference

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -6,6 +6,6 @@ then
 fi
 
 mono nuget.exe install sider
-mono nuget.exe install newtonsoft.json
+mono nuget.exe install newtonsoft.json -version 8.0.3
 mono nuget.exe install nunit -version 2.6.4
 mono nuget.exe install nunit.runners -version 2.6.4
