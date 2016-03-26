7cde7456e660f4b2031d5151e33961f1dbb396ac
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-26 13:54:07 +1100

    Fixed build from github script

--- build-from-github.sh
+++ build-from-github.sh
@@ -1,11 +1,15 @@
 BRANCH=$1
 
 if [ -z "$BRANCH" ]; then
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 
 echo "Branch: $BRANCH"
 
-git clone https://github.com/CompulsiveCoder/ipfs-cs.git --branch $BRANCH
-cd ipfs-cs
+git clone https://github.com/CompulsiveCoder/datamanager.git --branch $BRANCH
+cd datamanager
 sh init-build.sh
