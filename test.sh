a8b18b2cdadfed77f308e0baafa663d94d5192a1
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-26 15:01:47 +1100

    Updated test script

--- test.sh
+++ test.sh
@@ -8,8 +8,9 @@ echo "Testing datamanager project"
 echo "  Dir: $PWD"
 echo "  Category: $CATEGORY"
 
-if [ "Integration" = "$CATEGORY" ]; then
-  nohup sudo redis-server &
-fi
+# TODO: Remove if not needed. Disabled because it crashes travis ci test
+#if [ "Integration" = "$CATEGORY" ]; then
+#  nohup sudo redis-server &
+#fi
 
 mono lib/NUnit.Runners.2.6.4/tools/nunit-console.exe bin/Release/*.dll --include=$CATEGORY
