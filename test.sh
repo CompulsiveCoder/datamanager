b5c6ae6dc1b5594a0ae13562ea1932b607b920ef
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-26 14:51:14 +1100

    Updated test.sh

--- test.sh
+++ test.sh
@@ -8,4 +8,8 @@ echo "Testing ipfs-cs project"
 echo "  Dir: $PWD"
 echo "  Category: $CATEGORY"
 
-nohup sudo redis-server & mono lib/NUnit.Runners.2.6.4/tools/nunit-console.exe bin/Release/*.dll --include=$CATEGORY
+if [ "Integration" = "$CATEGORY" ]; then
+  nohup sudo redis-server
+fi
+
+mono lib/NUnit.Runners.2.6.4/tools/nunit-console.exe bin/Release/*.dll --include=$CATEGORY
