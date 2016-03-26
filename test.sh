b928e3eb6f102e6e153ab5f929f666964d0abe1b
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-26 15:00:15 +1100

    Updated script

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ if [ -z "$CATEGORY" ]; then
     CATEGORY="Unit"
 fi
 
-echo "Testing ipfs-cs project"
+echo "Testing datamanager project"
 echo "  Dir: $PWD"
 echo "  Category: $CATEGORY"
 
 if [ "Integration" = "$CATEGORY" ]; then
-  nohup sudo redis-server
+  nohup sudo redis-server &
 fi
 
 mono lib/NUnit.Runners.2.6.4/tools/nunit-console.exe bin/Release/*.dll --include=$CATEGORY
