7ab761d5f880ff95273cd1d8e979814fdbfd8707
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-26 15:08:21 +1100

    reeabled redis server

--- test.sh
+++ test.sh
@@ -8,9 +8,8 @@ echo "Testing datamanager project"
 echo "  Dir: $PWD"
 echo "  Category: $CATEGORY"
 
-# TODO: Remove if not needed. Disabled because it crashes travis ci test
-#if [ "Integration" = "$CATEGORY" ]; then
-#  nohup sudo redis-server &
-#fi
+if [ "Integration" = "$CATEGORY" ]; then
+  nohup sudo redis-server &
+fi
 
 mono lib/NUnit.Runners.2.6.4/tools/nunit-console.exe bin/Release/*.dll --include=$CATEGORY
