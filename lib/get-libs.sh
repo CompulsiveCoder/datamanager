85da50272ad0bb0257abba4dff8f49a62938eea4
CompulsiveCoder <compulsivecoder@gmail.com>
2016-04-02 21:58:18 +1100

    Fixed get-libs

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,22 +1,30 @@
 echo "Getting library files"
 echo "  Dir: $PWD"
 
+LIB_DIR=$PWD
+
 PARENT_DIR=../../..
 PARENT_LIB_DIR=$PARENT_DIR/lib
 
 echo "Parent libs directory: $PARENT_LIB_DIR"
 
 if [ -f $PARENT_LIB_DIR/nuget.exe ]; then
+   cd $PARENT_LIB_DIR
    echo "Copying libs from parent projects"
-   cp $PARENT_LIB_DIR/nuget.exe . -v
-   cp $PARENT_LIB_DIR/**/* . -rv
+   echo $PWD
+   cp -vr nuget.exe $LIB_DIR/
+   for d in */ ; do
+    cp -vr $d $LIB_DIR/$d
+   done
+
+   cd $DIR
 fi
 
 NUGET_FILE="nuget.exe"
 
 if [ ! -f "$NUGET_FILE" ];
 then
-    wget http://nuget.org/nuget.exe
+   wget http://nuget.org/nuget.exe
 fi
 
 mono nuget.exe install newtonsoft.json -version 8.0.3
