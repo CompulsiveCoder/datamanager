57f6be6845b37e34679f94c2784f473fcd613b9b
CompulsiveCoder <compulsivecoder@gmail.com>
2016-04-02 11:03:47 +1100

    Updated build-from-github script

--- build-from-github.sh
+++ build-from-github.sh
@@ -1,5 +1,7 @@
 BRANCH=$1
 
+DIR=$PWD
+
 if [ -z "$BRANCH" ]; then
     BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 fi
@@ -10,6 +12,13 @@ fi
 
 echo "Branch: $BRANCH"
 
-git clone https://github.com/CompulsiveCoder/datamanager.git --branch $BRANCH
-cd datamanager
-sh init-build.sh
+# If the .tmp/datamanager directory exists then remove it
+if [ -d "$BRANCH" ]; then
+    rm .tmp/datamanager -rf
+fi
+
+git clone https://github.com/CompulsiveCoder/datamanager.git .tmp/datamanager --branch $BRANCH
+cd .tmp/datamanager && \
+sh init-build.sh && \
+cd $DIR && \
+rm .tmp/datamanager -rf
