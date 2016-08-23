4c7b377b7a057e0300020a79cc2e455691ff8a7a
CompulsiveCoder <compulsivecoder@gmail.com>
2016-08-23 16:55:22 +1000

    Disabled redis launch during test.sh because it's causing problems with the terminal

--- test.sh
+++ test.sh
@@ -8,8 +8,9 @@ echo "Testing datamanager project"
 echo "  Dir: $PWD"
 echo "  Category: $CATEGORY"
 
-if [ "Integration" = "$CATEGORY" ]; then
-  nohup sudo redis-server &
-fi
+# TODO: Remove if not needed. Should redis be started here? Or within the test setup?
+#if [ "Integration" = "$CATEGORY" ]; then
+#  nohup sudo redis-server & # This ampersand is causing problems; redis runs in the background and hangs the terminal window
+#fi
 
 mono lib/NUnit.Runners.2.6.4/tools/nunit-console.exe bin/Release/*.dll --include=$CATEGORY
