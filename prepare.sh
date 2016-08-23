2415add27c309089946b937cf5e1a41be6eaafc5
CompulsiveCoder <compulsivecoder@gmail.com>
2016-08-23 17:15:49 +1000

    Tweaked prepare.sh and travis.yml

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,5 @@ echo "Preparing damanager project"
 echo "  Dir: $PWD"
 
 sudo apt-get update
-sudo apt-get install -y git wget mono-complete redis-server
-
-# Disabled because it breaks travis CI process # TODO Remove if not needed
-#mozroots --import --sync
+sudo apt-get install -y git wget mono-complete # redis-server
+# redis-server install disabled. It's not needed for testing because the redis client is mocked. It's up the the end user to decide whether or not to install/use it
