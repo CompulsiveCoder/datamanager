0713cdd6a7f33f328563d7b977ffac06c8c01e77
CompulsiveCoder <compulsivecoder@gmail.com>
2017-01-29 10:47:23 +1100

    Added missing provider base

---
+++ src/datamanager.Data/Providers/BaseDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace datamanager.Data.Providers
+{
+    public abstract class BaseDataProvider
+    {
+        public abstract string Get(string key);
+        public abstract string[] GetKeys();
+        public abstract string[] GetKeys(string pattern);
+
+        public abstract void Set(string key, string value);
+
+        public virtual void Append (string key, string value)
+        {
+            var existingValue = Get (key);
+
+            var newValue = existingValue += value;
+
+            Set (key, newValue);
+        }
+
+        public abstract void Delete(string key);
+
+        public abstract void DeleteAll();
+
+        public abstract void Quit();
+    }
+}
+
